729d8745cb2722beb3f7838cba978e47f791efe3
anshul <anshultamrakar487@gmail.com>
2024-07-09 15:39:02 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import "./"
 import Header from "./compenents/Header";
 import Body from "./compenents/Body";
+import Footer from "./compenents/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Body/>
+      <Footer/>
     </div>
   );
 }
--- src/compenents/Body.js
+++ src/compenents/Body.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Sidebar from './Sidebar'
+import  MainContainer from "./MainContainer"
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div>
+      <Sidebar/>
+      <MainContainer/>
+    </div>
   )
 }
 
