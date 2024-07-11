4b96214f9e3c573b6b4764d95e8b7c505f7ec236
anshul <anshultamrakar487@gmail.com>
2024-07-11 16:18:47 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import "./"
+
 import Header from "./compenents/Header";
 import Body from "./compenents/Body";
-import Footer from "./compenents/Footer";
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Body/>
-      <Footer/>
     </div>
   );
 }
--- src/compenents/Sidebar.js
+++ src/compenents/Sidebar.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='m-4'>
       <ul>
         <li>Home</li>
         <li>Shorts</li>
         <li>Subscription</li>
+      </ul>
+     
+      <ul>
+        <p className=''>You</p>
+        <li>History</li>
+        <li>Playlists</li>
+        <li>Watch Later</li>
+        <li>Liked videos</li>
       </ul>
     </div>
   )
