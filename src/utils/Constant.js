fa7596fb0c1d55c3156e2eedef0ad81f36240bd5
anshul <anshultamrakar487@gmail.com>
2024-07-25 17:55:06 +0530

    chnages

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,1 +1,4 @@
-const API = "https://www.googleapis.com/youtube/v3/videos"
+const GOOGLE_KEY = "AIzaSyAQf9ak1GB4MtVLCrojn-dWsJbnin0X-5I"
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" + GOOGLE_KEY
+
+
